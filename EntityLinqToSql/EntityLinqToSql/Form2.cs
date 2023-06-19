5bbf82edf7ae3493a4b3d262086cce19c2066705
kubrataskir <127491457+kubrataskir@users.noreply.github.com>
2023-06-19 20:43:31 +0300

    Add files via upload

---
+++ EntityLinqToSql/EntityLinqToSql/Form2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityLinqToSql
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+           /* KuzeyYeliDataContext dc = new KuzeyYeliDataContext();*/
+            /* var sonuc = from urun in dc.Urunlers
+                         join satisdetay in dc.SatisDetays on
+                         urun.UrunID equals satisdetay.UrunID
+                         group satisdetay by urun.UrunAdi into grup
+                         select new
+                         {
+                             UrunAdi = grup.Key,
+                             ToplamSatis = grup.Sum(x => x.Fiyat * x.Adet)
+                         };
+
+             dataGridView1.DataSource = sonuc;*/
+
+            //lazy load = hantal yükleme yöntemi
+           /* var sonuc2 = from urun in dc.Urunlers
+                         select new
+                         {
+                             urun.UrunAdi,
+                             ToplamSatis = urun.SatisDetays.
+                             Any() ? urun.SatisDetays.
+                             Sum(x => x.Fiyat * x.Adet) : 0//kayıt varsa yoklama alacak
+                                                           // yoksa 0 verecek
+                         };
+            dataGridView1.DataSource = sonuc2;*/
+        }
+    }
+}
---
+++ EntityLinqToSql/EntityLinqToSql/Form3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityLinqToSql
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            //her personelin kaç kayıt yaptığını sorgulatıyoruz
+            KuzeyYeliDataContext guney = new KuzeyYeliDataContext();
+            var sonuc = from satis in guney.Satislars
+                        join personel in guney.Personellers on
+                        satis.PersonelID equals
+                        personel.PersonelID
+                        group satis by personel.Adi into grup
+                        select new
+                        {
+                            PersonelAdi = grup.Key,
+                            ToplamSatis = grup.Count()
+                        };
+            dataGridView1.DataSource = sonuc;
+        }
+    }
+}
