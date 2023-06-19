b0ad7948eec346832240a42ca75e53a596ccfa30
kubrataskir <127491457+kubrataskir@users.noreply.github.com>
2023-06-19 20:34:04 +0300

    Add files via upload

---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            kanepe knp1= new kanepe();
+            masa calisma_masasi=new masa();
+            knp1.renk = "Siyah";
+            knp1.kumas = "Keten";
+            calisma_masasi.renk = "Kahverengi";
+            calisma_masasi.malzeme = "Ahşap";
+            knp1.ozellikyaz();
+            Console.WriteLine();
+            calisma_masasi.ozellikyaz();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ AbstractClass/AbstractClass/mobilya.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    abstract class mobilya
+    {
+        public string renk;
+
+        abstract public void ozellikyaz();
+
+    }
+    class kanepe : mobilya
+    {
+        public string kumas;
+
+        public override void ozellikyaz()
+        {
+            Console.WriteLine("Kanepenin Özellikleri");
+            Console.WriteLine("Renk: {0} ", renk);
+            Console.WriteLine("Kumaş: {0} ", kumas);
+        }
+    }
+    class masa : mobilya
+    {
+        public string malzeme;
+        public override void ozellikyaz() 
+        {
+            Console.WriteLine("Masanın Özellikleri");
+            Console.WriteLine("Renk: {0} ",renk);
+            Console.WriteLine("Malzeme: {0} ",malzeme);
+        }
+
+    }
+
+
+
+
+
+
+}
