1c947420cc7544b24a463f2d92dfe62a73aec58c
kubrataskir <127491457+kubrataskir@users.noreply.github.com>
2023-06-19 20:41:26 +0300

    Add files via upload

---
+++ DisconnectedMimari/DisconnectedMimari/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DisconnectedMimari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection("Data Source=103A-10;Initial Catalog=KuzeyYeli;Integrated Security=True");
+
+
+        private void NewMethod()
+        {
+            SqlDataAdapter adp = new SqlDataAdapter("Select*from Urunler", baglanti);
+            DataTable dt= new DataTable();
+            adp.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns["UrunID"].Visible = false;//kolon gizler
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            NewMethod();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string adi = textBox1.Text;
+            decimal fiyat = numericUpDown1.Value;
+            decimal stok=numericUpDown2.Value;
+            if (adi==""|| fiyat==0 || stok==0)
+            {
+                MessageBox.Show("Tüm alanları eksiksiz doldurun!!");
+            }
+            SqlCommand komut = new SqlCommand();
+            komut.CommandText = string.Format("Insert Urunler(UrunAdi,Fiyat,Stok) values('{0}','{1}','{2}')",adi,fiyat,stok);
+            komut.Connection = baglanti;
+            baglanti.Open();
+            int etkilenen=komut.ExecuteNonQuery();
+            if (etkilenen>0)
+            {
+                MessageBox.Show("Kayıt Eklendi!!");
+                NewMethod();
+            }
+            else
+            {
+                MessageBox.Show("Kaydınız Eklenemedi!!");
+            }
+
+            baglanti.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlCommand command =new SqlCommand();
+            command.CommandText = string.Format("update Urunler set UrunAdi='{0}',Fiyat={1},Stok={2} where UrunID={3}",
+                textBox1.Text, numericUpDown1.Value, numericUpDown2.Value, textBox1.Tag);
+            command.Connection = baglanti;
+            baglanti.Open();
+
+            try
+            {
+                int etk=command.ExecuteNonQuery();
+                baglanti.Close();
+                if (etk>0)
+                {
+                    MessageBox.Show("Kayıt Güncellendi");
+                    NewMethod();
+                }
+                else
+                {
+                    MessageBox.Show("Kayıt Güncellenirken hata oluştu");
+                }
+            }
+            catch (Exception ex)
+            {
+                baglanti.Close();
+                MessageBox.Show("Kayıt güncellenirken hata oluştu"+ex.Message);
+            }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1.Text = dataGridView1.CurrentRow.Cells["UrunAdi"].Value.ToString();
+            textBox1.Tag = dataGridView1.CurrentRow.Cells["UrunID"].Value;
+            numericUpDown1.Value = (decimal)dataGridView1.CurrentRow.Cells["Fiyat"].Value;
+            numericUpDown2.Value = Convert.ToDecimal(dataGridView1.CurrentRow.Cells["Stok"].Value);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            KategoriForm ktgr=new KategoriForm();
+            ktgr.ShowDialog();
+            
+        }
+    }
+}
---
+++ DisconnectedMimari/DisconnectedMimari/KategoriForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace DisconnectedMimari
+{
+    partial class KategoriForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KategoriAdi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(156, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Tanimi";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(334, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 130);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 275);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(159, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // KategoriForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "KategoriForm";
+            this.Text = "Kategori";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
